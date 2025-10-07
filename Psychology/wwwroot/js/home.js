4d6ae3a4d669cf4c0f67b258d95d3ab9a05cef35
Mohammad.chz <mohammad.chz1378@gmail.com>
2025-10-07 20:44:16 +0330

    Add question section

--- Psychology/wwwroot/js/home.js
+++ Psychology/wwwroot/js/home.js
@@ -8,6 +8,11 @@
     }, { threshold: 0.3 });
 
     observer.observe(section);
+
+    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
 });
 
 function animateCounter(el, duration = 2000) {
